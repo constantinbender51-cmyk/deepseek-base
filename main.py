50998938d1c383b8d606b4f77aff89492d2125f3
Constantin Bender <constantinbender51@gmail.com>
2026-02-19 16:05:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import replicate
 
-input = {"the thing you habg your coat on so it doesn't get wrinkls is called a"}
+input = {"input": "the thing you habg your coat on so it doesn't get wrinkls is called a"}
 
 for event in replicate.stream(
     "deepseek-ai/deepseek-67b-base:0f2469607b150ffd428298a6bb57874f3657ab04fc980f7b5aa8fdad7bd6b46b",
@@ -8,3 +8,4 @@ for event in replicate.stream(
 ):
     print(event, end="")
     #=> " computed"
+
