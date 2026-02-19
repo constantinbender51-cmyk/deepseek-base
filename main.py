d64ecb9bf6b54b28d540480248301e677153aa94
Constantin Bender <constantinbender51@gmail.com>
2026-02-19 16:22:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import replicate
 
 outputs = replicate.run(
     "deepseek-ai/deepseek-67b-base:0f2469607b150ffd428298a6bb57874f3657ab04fc980f7b5aa8fdad7bd6b46b",
-    input={"input": "Write a short story about a robot learning to paint."}
+    input={"input": "WThe the you hang clothes so they don't wrinkle is called a "}
 )
 
 # If outputs is a generator of strings
